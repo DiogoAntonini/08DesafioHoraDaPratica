09cb0057d13974d771533cf034c00fb8f5452b0d
xxx <xxx@XXX>
2024-02-03 16:53:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		List<string> list = new List<string>();
+
+		list.Add ("C#");
+		list.Add("Java");
+		list.Add("JavaScript");
+
+		Console.WriteLine(list[0]);
+
+		Console.ReadKey();
+	}
+}
